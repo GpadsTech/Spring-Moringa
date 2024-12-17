40963cf8f60190d588226a7166f3eb3b732de9d1
WemmersonAlb <wemmerson.albuquerque@gmail.com>
2024-12-17 08:41:12 -0300

    Endereço do cors police corrigido

--- src/main/java/com/gpads/moringa/config/CorsConfig.java
+++ src/main/java/com/gpads/moringa/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500/")
+                        .allowedOrigins("https://interface-morhinga.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
